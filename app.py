470b612932e073176ae451b85efa2c3e3b1295ae
JanettValadez <majaakbal@gmail.com>
2025-10-05 16:23:55 +0200

    instalar plotly y volver a correr script

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-import plotly_express as px
+import plotly.express as px
 
 df_cars = pd.read_csv('C:/Users/maja_/tripleten/projects/Proyecto-7/vehicles_us.csv')\
 
