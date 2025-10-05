ffdcdbaf0b82c1e39b67507b82413176f53cd087
JanettValadez <majaakbal@gmail.com>
2025-10-05 13:26:46 +0200

    Se depuran librerias

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ import plotly_express as px
 
 df_cars = pd.read_csv('C:/Users/maja_/tripleten/projects/Proyecto-7/vehicles_us.csv')\
 
+st.header('Anuncio de coches')
+hist_button = st.button('Construir Histograma')
 
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de autos en Norte América')
+    fig_hist = px.histogram(df_cars, x='odometer')
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+scatter_button = st.button('Construir Diagrama de Dispersión')
+
+if scatter_button:
+    st.write('Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de autos en Norte América')
+    fig_scatter = px.scatter(df_cars, x="odometer", y='price')
+    st.plotly_chart(fig_scatter)
 
 
 
